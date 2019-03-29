ad358b067d955b0394e0198a6744138db5316785
Luke King <lkingfrm@gmail.com>
2019-03-28 22:01:34 -0600

    first commit

---
+++ Models/LocalLib.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Models
+{
+    class LocalLib
+    {
+        public string Location { get; private set; }
+        public string Name { get; private set; }
+
+        //list of books in Library and books checked out
+        List<Book> Books { get; set; }
+        List<Book> CheckedOut { get; set; }
+
+
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public string checkOutBook(int index)
+        {
+            Book book = Books[index];
+
+            if (Books.Contains(book))
+            {
+                CheckedOut.Add(book);
+                Books.Remove(book);
+                return $"{book.Name}";
+            }
+            return $"{book.Name} isn't available";
+        }
+
+        public Book infoBook(int index)
+        {
+            return Books[index];
+        }
+
+        public bool checkBook(int index)
+        {
+            if (Books.Contains(Books[index]))
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public void PrintBooks()
+        {
+            for (int i = 0; i < Books.Count; i++)
+            {
+            Console.WriteLine($"{i+1}-{Books[i].Name} by {Books[i].Author}");
+
+            }
+        }
+        
+        public LocalLib(string location, string name)
+        {
+            Location = location;
+            Name = name;
+            Books = new List<Book>();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Library.Models;
+using System.Collections.Generic;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book silverstien1 = new Book("Shell Silverstein", "Where the SideWalk Ends");
+            Book tolkien = new Book("J.R.R. Tolkien","The Lord Of The Rings" );
+            Book lewis = new Book("C.S. Lewis", "The Lion The Witch and the Wardrobe");
+            Book rowling = new Book("J.K. Rowling", "Harry Potter and the Gobblet Of Fire");
+
+            LocalLib caldwell = new LocalLib("Caldwell, Id", "Caldwell Library");
+            caldwell.AddBook(silverstien1);
+            caldwell.AddBook(tolkien);
+            caldwell.AddBook(lewis);
+            caldwell.AddBook(rowling);
+
+            Console.WriteLine($"Welcome to the {caldwell.Name} in {caldwell.Location}");
+
+            colorOutput("Available Books", "Yellow");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            caldwell.PrintBooks();
+
+            Console.ResetColor();
+
+            Console.WriteLine("Please select a book number to checkout");
+            string requestString = Console.ReadLine();
+            int outIndex = checkInputValReturnNum(requestString);
+            if (caldwell.checkBook(outIndex))
+            {
+                colorOutput($"You have chosen {caldwell.infoBook(outIndex).Name}. Continue \"Y\"", "Yellow");
+                string answer = Console.ReadLine().ToUpper();
+                if ( answer == "Y")
+                {
+                    colorOutput($"Result: {caldwell.checkOutBook(outIndex)}", "Blue");
+                }else
+                {
+                    colorOutput("Responce not \"Y\". Book not checked out. Thankyou.", "Red");
+                }
+            }
+        }
+        static int checkInputValReturnNum(string input)
+        {
+            int result;
+            if (Int32.TryParse(input, out result))
+            {
+                result = Int32.Parse(input);
+                return result -1;
+            }
+            return -1;
+        }
+
+        static void colorOutput(string output,string color)
+        {
+            ConsoleColor consoleColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color, true);
+           
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(output);
+            Console.ResetColor();
+
+        }
+    }
+}
